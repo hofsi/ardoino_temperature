df1c2b0342e221489deea2abe68fa99836e58d3e
hofsi <simon@hofinger.re>
2024-06-13 19:29:51 +0200

    Implemented Interrupt

--- src/i2c.c
+++ src/i2c.c
@@ -1,5 +1,107 @@
 #include "i2c.h"
 #include "serial.h"
+#include <cstdint>
+
+typedef enum TWCStage{
+    Start, SlaveAddress, TR, Stop, Off
+}TWCStage;
+
+typedef enum TWCMode{
+    Transmit, Recive,
+} TWCMode;
+
+
+typedef struct TWCStruct{
+    uint8_t *address;
+    TWCStage stage;
+    TWCMode mode;
+    uint8_t *data;
+    uint8_t length;
+    uint8_t position;
+} TWCStruct;
+
+TWCStruct twcstruct;
+
+void twc_init(){
+    TWBR = 0b10000000;
+    TWSR |= 0b00000000;
+
+    /* Enable internal pull-ups */
+    DDRC  &= ~((1<<PC5) | (1<<PC4));
+    PORTC |=   (1<<PC5) | (1<<PC4);
+
+    sei();
+}
+
+uint8_t twc_start(TWCStruct twcs){
+    if (twcs.stage != Start){
+	serialWrite("ETWC No Start\n\r");
+	return 1;
+    }
+    twcstruct = twcs;
+    sei();
+    TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN)|(1<<TWIE);
+}
+
+ISR(TWI_vect){
+    /*Check for error*/
+    if (!(((TWSR & 0xF8) == 0x08) ||
+	((TWSR & 0xF8) == 0x18) ||
+	((TWSR & 0xF8) == 0x28) ||
+	((TWSR & 0xF8) == 0x10) ||
+	((TWSR & 0xF8) == 0x40) ||
+	((TWSR & 0xF8) == 0x50) ||
+	((TWSR & 0xF8) == 0x58)
+	)){
+	serialWrite("ETWC2 Transmission Failed ");
+	char result[6];
+	sprintf(result,"%i\n\r", TWSR);
+	serialWrite(result);
+	return;
+    }
+
+    /*Select Next Stage*/
+   twc_next();
+    /*Begin Transmission*/
+}
+
+void twc_stop(){
+    TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
+}
+
+bool twc_tr(){
+    twcstruct.stage = TR;
+    if twcstruct.mode == Transmit{
+	TWDR = twcstruct.data[twcstruct.position];
+    } else {
+	twcstruct.data[twcstruct.position] = TWDR;
+	TWCR = (1<<TWEA);
+    }
+    twcstruct.position++;
+    if twcstruct.position => twcstruct.length{
+	TWCR = 0;
+	twcstruct.stage = Stop;
+    }
+}
+
+void twc_next(){
+    switch (twcstruct.stage) {
+	case Start:
+	    TWDR = twcstruct.address;
+	case SlaveAddress:
+	case TR:
+	    twc_tr();
+	case Stop:
+	    twcstruct.stage = Off;
+	    TWCR = (1<<TWSTO);
+	default:
+	    TWCR = 0;
+	    return;
+    }
+    TWCR &= (1<<TWINT)|(1<<TWEN)|(1<<TWIE);
+}
+
+//=============OLD=CODE=========================
 
 void end_transmission(){
     TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
@@ -36,7 +138,10 @@ void initTWC(){
     // Enable internal pull-ups
     DDRC  &= ~((1<<PC5) | (1<<PC4));
     PORTC |=   (1<<PC5) | (1<<PC4);
+
+    sei();
 }
+
 
 uint8_t TWC_MT(uint8_t _slave_addr, uint8_t* _data, uint8_t _data_len ){
     uint8_t result = start_transmission();
